446f35e032f21e10eb665b6aa93f830cc1029669
maokebing <kebing1011@163.com>
2015-09-06 21:07:35 +0800

    Initial Commit

---
+++ ViewLifecycle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewLifecycle
+//
+//  Created by mao on 9/6/15.
+//  Copyright (c) 2015 Maokebing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewLifecycle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewLifecycle
+//
+//  Created by mao on 9/6/15.
+//  Copyright (c) 2015 Maokebing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
