b89a532539f4943ac2eb200c6ee0e0d75e09d879
maokebing <kebing1011@163.com>
2015-09-13 12:40:29 +0800

    first commit

---
+++ ViewLifecycle/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  ViewLifecycle
+//
+//  Created by mao on 9/6/15.
+//  Copyright (c) 2015 Maokebing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ ViewLifecycle/UIView+MISVFL.m
@@ -0,0 +1,21 @@
+//
+//  UIView+MISVFL.m
+//  ViewLifecycle
+//
+//  Created by mao on 9/6/15.
+//  Copyright (c) 2015 Maokebing. All rights reserved.
+//
+
+#import "UIView+MISVFL.h"
+
+@implementation UIView_MISVFL
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
